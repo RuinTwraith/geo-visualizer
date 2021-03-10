731c6162ffa9946517ba4f6c84df71af46447ba8
Dibbendu Koley <dibbenduk@gmail.com>
2021-03-10 12:23:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import GetFeatured from "./components/GetFeatured";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Search Bar</h1>
+      <h1>Test 4</h1>
+      <GetFeatured/>
+    </div>
+  );
+}
+
+export default App;
